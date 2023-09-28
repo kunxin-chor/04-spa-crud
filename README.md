92d2dd2e73bd6db5bb79e1ef731b25d90a3f88e7
Paul <chorkunxin@gmail.com>
2023-09-28 10:09:34 +0800

    Added list rendering

--- README.md
+++ README.md
@@ -7,5 +7,8 @@
   - import in the pages
   - in the App component, in its JSX set up the routing
 
-  ## Show all the reviews
-  1. 
+  ## Branch 01: Show all the reviews
+  1. We add mock reviews in the state for the ShowReview.jsx (later we refactor to use a context api)
+  2. Do list rendering
+    - we can use a function that returns an array of jsx elements
+    - we can use .map directly in the jsx
--- src/pages/ShowReviews.jsx
+++ src/pages/ShowReviews.jsx
@@ -1,7 +1,71 @@
+import { useState } from "react";
+
 export default function ShowReviews() {
-    return (
-        <>
-        <h1>All Reviews</h1>
-        </>
-    )
+  const [reviews, setReviews] = useState([
+    {
+      _id: "64931f23755a1426a2343234",
+      restaurant: "Tian Tian Chicken Rice2222",
+      review: "Nice rice and juicy meat",
+      rating: 5,
+    },
+    {
+      _id: "651264db7841ce1f3dedd662",
+      restaurant: "Taco Bell",
+      review: "Great tacos!",
+      rating: 3,
+    },
+    {
+      _id: "651264db7841ce1f3dedd663",
+      restaurant: "Arnold's Chicken",
+      review: "Great fried chicken!",
+      rating: 5,
+    },
+  ]);
+
+  const renderReviews = () => {
+    const results = [];
+    for (let r of reviews) {
+      const eachReview = (
+        <div key={r._id}>
+          <h3>{r.restaurant}</h3>
+          <p>{r.review}</p>
+          <p>Rating: {r.rating}/5</p>
+        </div>
+      );
+
+      results.push(eachReview);
+    }
+    return results;
+  };
+  // How do render any list of JSX elements
+  //   // storing a JSX object in a variable
+  // const header = <h2>All Reviews</h2>;
+
+  // // if a variable can store JSX, then it stands to reason we can store JSX in an array
+  // const list = [
+  //     <li>Apples</li>,
+  //     <li>Oranges</li>,
+  //     <li>Bananas</li>
+  // ]
+
+  return (
+    <>
+      <h2>All Reviews</h2>
+      {renderReviews()}
+
+
+      {
+        // returns a new array
+        // each element of the new array is transformed an element in an array
+        reviews.map((r,index)=>{
+            return  (<div key={r._id}>
+            <h3>{r.restaurant}</h3>
+            <p>{r.review}</p>
+            <p>Rating: {r.rating}/5</p>
+          </div>);
+        })
+
+      }
+    </>
+  );
 }
